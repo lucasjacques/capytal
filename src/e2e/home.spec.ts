0cd4c9119479edbd2a26fa618796ae4d6c30ffe4
Lucas Jacques <lucas.jacs@gmail.com>
2026-06-25 14:14:11 -0300

    test: update heading to welcome back in login page render

--- src/e2e/home.spec.ts
+++ src/e2e/home.spec.ts
@@ -5,7 +5,9 @@ test("redirects unauthenticated users to login", async ({ page }) => {
   await expect(page).toHaveURL(/\/login/);
 });
 
-test("login page renders sign in heading", async ({ page }) => {
+test("login page renders welcome heading", async ({ page }) => {
   await page.goto("/login");
-  await expect(page.getByRole("heading", { name: /sign in/i })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: /welcome back/i }),
+  ).toBeVisible();
 });
